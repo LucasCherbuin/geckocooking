0f485f21c49c50e26310482542c2e31fae6d67ba
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-09-30 15:24:49 +0200

    configuration de scss dans le projet

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< HEAD
 const nextConfig = {};
+=======
+const nextConfig = {
+  //react
+  reactStrictMode: true,
+  //sass
+  sassOptions: {
+    implementation: 'sass-embedded'
+  },
+};
+>>>>>>> db55946 (configuration de scss dans le projet)
 
 export default nextConfig;
