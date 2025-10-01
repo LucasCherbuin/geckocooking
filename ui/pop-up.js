4c2dfe0ec41bc777a17bf0a7307d8f04e162d959
unknown <lucas.cherbuin@gmail.com>
2025-10-01 17:15:00 +0200

    configuration du mailer lors d'une inscription

---
+++ ui/pop-up.js
@@ -0,0 +1,10 @@
+import { User } from '@phosphor-icons/react';
+
+export function confirmation() {
+    return (
+    <div>
+        <p> Un mail de confirmation vous a été envoyé</p>
+        <button type="button"><CheckCircle size={32} /></button>
+    </div>
+    );
+}
--- ui/signup-form.js
+++ ui/signup-form.js
@@ -1,41 +1,59 @@
 import { signup } from '@/actions/auth'
 import * as  sass from  '../sass/'
+import { confirmation } from 'pop-up'
 
 export function SignupForm() {
     const [state, action, pending] = useActionState(signup, undefined)
 
     return (
-        <form action={signup}>
-            <div>
-                <label htmlFor="pseudo">Nom d'utilisateur</label>
-                <input id="pseudo" name="pseudo" placeHolder="Nom d'utilisateur"></input>
-            </div>
-            {state?.error?.pseudo && <p>{state.errors.pseudo}</p>}
-            <div>
-                <label htmlFor="email">Email</label>
-                <input id="email" name="email" placeHolder="Email"></input>
-            </div>
-            {state?.error?.email && <p>{state.errors.email}</p>}
-            <div>
-                <label htmlFor="motDePasse">Mot de passe</label>
-                <input id="motDePasse" name="motDePasse" placeHolder="Mot de passe"></input>
-            </div>
-            {state?.error?.motDePasse && (
+        <div>
+            <form action={signup}>
                 <div>
-                    <p>motDePasse must:</p>
-                    <ul>
-                        {state.errors.motDePasse.map ((error) => (
-                            <li key={error}>- {error}</li>
-                        ))}
-                    </ul>
+                    <label htmlFor="pseudo">Nom d'utilisateur</label>
+                    <input id="pseudo" name="pseudo" placeHolder="Nom d'utilisateur"></input>
                 </div>
-            )}
-            <div>
-                <input Type="checkbox" name='remember'>Maintenir la connexion</input>
-            </div>
-            <div>
-                <button disabled={pending} type="submit">s'inscrire</button>
-            </div>
-        </form>
+                {state?.error?.pseudo && <p>{state.errors.pseudo}</p>}
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" placeHolder="Email"></input>
+                </div>
+                {state?.error?.email && <p>{state.errors.email}</p>}
+                <div>
+                    <label htmlFor="motDePasse">Mot de passe</label>
+                    <input id="motDePasse" name="motDePasse" placeHolder="Mot de passe"></input>
+                </div>
+                {state?.error?.motDePasse && (
+                    <div>
+                        <p>motDePasse must:</p>
+                        <ul>
+                            {state.errors.motDePasse.map ((error) => (
+                                <li key={error}>- {error}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                <div>
+                    <input Type="checkbox" name='remember'>Maintenir la connexion</input>
+                </div>
+                <div>
+                    <button disabled={pending} type="submit">s'inscrire</button>
+                </div>
+            </form>
+            <confirmation />
+        </div>        
     )
+    async function mailConfirmation {
+        try {
+            const mailoptions = {
+                from: process.env.FROM_EMAIL,
+                to: utilisateur.email,
+                subject: "votre inscription sur GeckoCooking",
+                html: "Bonjour ${pseudo} votre compte à bien été crée"
+            }
+
+            await WebTransportError.sendmail()
+
+            
+        } catch (error ) 
+    }
 }
